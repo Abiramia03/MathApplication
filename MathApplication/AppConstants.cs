dea441facb28526c651e5d3fc42016d4a1e51a32
Abiramia03 <abirami.abi03@gmail.com>
2022-07-02 15:15:16 +0100

    Code Refactoring

--- MathApplication/AppConstants.cs
+++ MathApplication/AppConstants.cs
@@ -7,9 +7,9 @@ namespace MathApplication
         public static double Pi = 3.14;
         public static int Two = 2;
         public static string Circle = "Circle";
-        public static string Equilateral = "Equilateral";
-        public static string Isosceles = "Isosceles";
-        public static string Scalene = "Scalene";
+        public static string Equilateral = "Equilateral Triangle";
+        public static string Isosceles = "Isosceles Triangle";
+        public static string Scalene = "Scalene Triangle";
         public static string Square = "Square";
         public static string Rectangle = "Rectangle";
         public static string Triangle = "Triangle";
